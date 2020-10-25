a2b4fdbae896d49e520af338d64067f0ab2739a7
amanaggarwal1 <amanaggarwal2013@gmail.com>
2020-10-25 17:28:40 +0530

    Getting memes with API calls using Volley library and loading images with the help of Glide library.

--- app/src/main/java/com/amanaggarwal1/memeshare/MainActivity.kt
+++ app/src/main/java/com/amanaggarwal1/memeshare/MainActivity.kt
@@ -2,10 +2,48 @@ package com.amanaggarwal1.memeshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
+
     }
+
+    private fun loadMeme(){
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        // Request a json response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val url = response.get("url")
+                Glide.with(this)
+                    .load(url)
+                    .into(memeIV)
+
+            },
+            {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+
+    }
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {}
 }
