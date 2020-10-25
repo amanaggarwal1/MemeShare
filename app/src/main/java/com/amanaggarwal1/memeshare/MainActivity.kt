aa1a38245182dabd22d3b81fa5ad85145ca84075
amanaggarwal1 <amanaggarwal2013@gmail.com>
2020-10-25 18:04:46 +0530

    Added progress bar while loading the image

--- app/src/main/java/com/amanaggarwal1/memeshare/MainActivity.kt
+++ app/src/main/java/com/amanaggarwal1/memeshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.amanaggarwal1.memeshare
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,10 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -21,6 +26,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadMeme(){
 
+        progressBar.visibility = View.VISIBLE
+
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
@@ -32,6 +39,30 @@ class MainActivity : AppCompatActivity() {
                 val url = response.get("url")
                 Glide.with(this)
                     .load(url)
+                    .listener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBar.visibility = View.GONE
+                            Toast.makeText(this@MainActivity, "There was an error", Toast.LENGTH_SHORT).show()
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBar.visibility = View.GONE
+                            return false
+                        }
+
+                    })
                     .into(memeIV)
 
             },
